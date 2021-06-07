8c3ef0659489f924a51530a6ff56d4c30af781d1
orkol1988 <75020439+orkol1988@users.noreply.github.com>
2021-06-07 17:25:59 +0300

    Update DemoApplication.java

--- demo_Project1_3_6_21/src/main/java/com/example/demo/DemoApplication.java
+++ demo_Project1_3_6_21/src/main/java/com/example/demo/DemoApplication.java
@@ -8,13 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-
-		RestaurantsREPO restaurantsREPO = new RestaurantsREPO("jdbc:sqlite:D:\\SQLiteDB\\Project_3_6_21\\ProjectRestaurant.db");
-
-		RestaurantsController.reset_m_Restaurants();
-
-		System.out.println(restaurantsREPO.getAllRestaurants());
 	}
 
 }
